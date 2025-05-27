b7098c60405b7150930f2ff1cc913cc9d3b58e5c
Suleyman Yildiz <suleymanyildiz491@gmail.com>
2025-05-27 22:11:34 +0300

    Add project files.

---
+++ BlogStore.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,23 @@
+﻿using BlogStore.EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogStore.DataAccessLayer.Context
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-9TE47SL\\SQLEXPRESS;initial catalog=BlogStoreNightDb;integrated Security=true; trust server certificate=true");
+        }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
