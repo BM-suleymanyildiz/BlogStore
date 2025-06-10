faed07d39a2382108215159b6070e24e1b0ab83e
Suleyman Yildiz <suleymanyildiz491@gmail.com>
2025-06-10 21:17:46 +0300

    ...

---
+++ BlogStore.PresentationLayer/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BlogStore.BusinessLayer.Abstract;
+using BlogStore.EntityLayer.Entities;
+namespace BlogStore.PresentationLayer.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        public IActionResult CommentList()
+        {
+            var values = _commentService.TGetAll();
+            return View(values);
+        }
+        [HttpGet]
+
+        public IActionResult CreateComment()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateComment(Comment comment)
+        {
+            _commentService.TInsert(comment);
+            return RedirectToAction("CommentList");
+        }
+    }
+}
