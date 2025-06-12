4941d27ab5e7e47718180de6e4191b57da1cbf2f
Suleyman Yildiz <suleymanyildiz491@gmail.com>
2025-06-12 20:43:48 +0300

    ...

---
+++ BlogStore.PresentationLayer/ViewComponents/_ScriptLayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogStore.PresentationLayer.ViewComponents
+{
+    public class _ScriptLayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
